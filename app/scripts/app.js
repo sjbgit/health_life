d2341c87b38a736f807b18d6efef888f308493cf
sjbgit <sjb_strat@hotmail.com>
2015-02-25 15:35:42 -0600

    start

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name healthLifeApp
+ * @description
+ * # healthLifeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('healthLifeApp', []);
